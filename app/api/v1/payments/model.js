88af0eccf1fb88e75a0247d3a146f02646291b56
Muhamad Nur Syami <nursami385@gmail.com>
2023-01-12 23:23:21 +0700

    CheckoutApi : ServiceParticipants CheckoutOrder & Model Payments

---
+++ app/api/v1/payments/model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const PaymentSchema = new mongoose.Schema(
+  {
+    // type ini seperti nama bank, kayak bri bni
+    type: {
+      type: String,
+      required: [true, "Tipe pembayaran harus diisi"],
+      minlength: 3,
+      maxlength: 50,
+    },
+    image: {
+      type: mongoose.Types.ObjectId,
+      ref: "Image",
+      required: true,
+    },
+    //metode pembayaran yang mana mau di bayar
+    status: {
+      type: Boolean,
+      enum: [true, false],
+      default: true,
+    },
+    organizer: {
+      type: mongoose.Types.ObjectId,
+      ref: "Organizer",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Payment", PaymentSchema);
